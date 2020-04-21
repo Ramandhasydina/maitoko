9044c9c16c518dd02556812d01192e7e1268ae59
Ramandhasydina <63191634+Ramandhasydina@users.noreply.github.com>
2020-04-21 11:45:05 +0700

    Update batiku.sql

--- batiku.sql
+++ batiku.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 07 Jun 2017 pada 01.22
--- Versi Server: 10.1.16-MariaDB
--- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,10 +8,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `batiku`
-
--- Struktur dari tabel `tabel_admin`
 
 
 CREATE TABLE `tabel_admin` (
@@ -29,27 +17,18 @@ CREATE TABLE `tabel_admin` (
   `password` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tabel_admin`
---
 
 INSERT INTO `tabel_admin` (`idAdmin`, `namaAdmin`, `email`, `password`) VALUES
 ('1', 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3');
 
--- --------------------------------------------------------
 
---
--- Struktur dari tabel `tabel_kategori`
---
 
 CREATE TABLE `tabel_kategori` (
   `idKategori` int(11) NOT NULL,
   `namaKategori` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tabel_kategori`
---
+
 
 INSERT INTO `tabel_kategori` (`idKategori`, `namaKategori`) VALUES
 (1, 'Pria'),
@@ -58,11 +37,7 @@ INSERT INTO `tabel_kategori` (`idKategori`, `namaKategori`) VALUES
 (4, 'Couple'),
 (5, 'Sarimbit');
 
--- --------------------------------------------------------
 
---
--- Struktur dari tabel `tabel_keranjang`
---
 
 CREATE TABLE `tabel_keranjang` (
   `idKeranjang` varchar(11) NOT NULL,
@@ -73,11 +48,7 @@ CREATE TABLE `tabel_keranjang` (
   `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Struktur dari tabel `tabel_komentar`
---
 
 CREATE TABLE `tabel_komentar` (
   `idKomen` int(11) NOT NULL,
@@ -86,11 +57,6 @@ CREATE TABLE `tabel_komentar` (
   `pesan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tabel_produk`
---
 
 CREATE TABLE `tabel_produk` (
   `idProduk` int(11) NOT NULL,
@@ -105,9 +71,6 @@ CREATE TABLE `tabel_produk` (
   `size` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tabel_produk`
---
 
 INSERT INTO `tabel_produk` (`idProduk`, `nama`, `gambar`, `ukuran`, `keterangan`, `kategori`, `harga`, `stock`, `path`, `size`) VALUES
 (15, 'Batik Sarimbit Keluarga', '185283bd07c8f6587b5a9b79c1a29661', 'm', 'Batik Sarimbit keluarga berkah', 'sarimbit', 500000, 100000, 'image/185283bd07c8f6587b5a9b79c1a29661.png', 265674),
@@ -117,12 +80,6 @@ INSERT INTO `tabel_produk` (`idProduk`, `nama`, `gambar`, `ukuran`, `keterangan`
 (19, 'Batik Couple', '359c2067e776e80ca60c5de15a1ca597', 's', 'Batik Couple', 'couple', 300000, 4, 'image/359c2067e776e80ca60c5de15a1ca597.png', 14872),
 (20, '', 'c2b25cd4cf155c98b1bfe2e646589028', 'l', 'Batik Sarimbit Keluarga Berkah', 'sarimbit', 123000, 2, 'image/c2b25cd4cf155c98b1bfe2e646589028.png', 91850);
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tabel_transaksi`
---
-
 CREATE TABLE `tabel_transaksi` (
   `idTransaksi` int(11) NOT NULL,
   `idUser` varchar(11) NOT NULL,
@@ -131,19 +88,13 @@ CREATE TABLE `tabel_transaksi` (
   `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tabel_transaksi`
---
 
 INSERT INTO `tabel_transaksi` (`idTransaksi`, `idUser`, `daftarBarang`, `tanggal`, `total`) VALUES
 (3, '1', '0', '0000-00-00', 2623000),
 (4, '1', '0', '2017-06-07', 123000),
 (5, '1', '0', '2017-06-07', 100000);
 
--- --------------------------------------------------------
 
---
--- Struktur dari tabe
 
 CREATE TABLE `tabel_trolly` (
   `idTrolly` int(11) NOT NULL,
@@ -162,20 +113,10 @@ CREATE TABLE `tabel_user` (
   `telpon` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tabel_user`
---
 
 INSERT INTO `tabel_user` (`idUser`, `namaUser`, `email`, `password`, `alamat`, `telpon`) VALUES
 (1, 'user1', 'user1@gmail.com', '24c9e15e52afc47c225b757e7bee1f9d', 'surabaya', '234234');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tabel_admin`
---
 ALTER TABLE `tabel_admin`
   ADD PRIMARY KEY (`idAdmin`);
 
@@ -185,83 +126,43 @@ ALTER TABLE `tabel_admin`
 ALTER TABLE `tabel_kategori`
   ADD PRIMARY KEY (`idKategori`);
 
---
--- Indexes for table `tabel_keranjang`
---
 ALTER TABLE `tabel_keranjang`
   ADD PRIMARY KEY (`idKeranjang`);
 
---
--- Indexes for table `tabel_komentar`
---
 ALTER TABLE `tabel_komentar`
   ADD PRIMARY KEY (`idKomen`);
 
---
--- Indexes for table `tabel_produk`
---
 ALTER TABLE `tabel_produk`
   ADD PRIMARY KEY (`idProduk`);
 
---
--- Indexes for table `tabel_transaksi`
---
 ALTER TABLE `tabel_transaksi`
   ADD PRIMARY KEY (`idTransaksi`);
 
---
--- Indexes for table `tabel_trolly`
---
 ALTER TABLE `tabel_trolly`
   ADD PRIMARY KEY (`idTrolly`);
 
---
--- Indexes for table `tabel_user`
---
 ALTER TABLE `tabel_user`
   ADD PRIMARY KEY (`idUser`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tabel_komentar`
---
 ALTER TABLE `tabel_komentar`
   MODIFY `idKomen` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tabel_produk`
---
+
+ AUTO_INCREMENT for table `tabel_produk`
+
 ALTER TABLE `tabel_produk`
   MODIFY `idProduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
---
--- AUTO_INCREMENT for table `tabel_transaksi`
---
+
+AUTO_INCREMENT for table `tabel_transaksi`
+
 ALTER TABLE `tabel_transaksi`
   MODIFY `idTransaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `tabel_trolly`
---
+AUTO_INCREMENT for table `tabel_trolly`
 ALTER TABLE `tabel_trolly`
   MODIFY `idTrolly` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `tabel_user`
---
+ AUTO_INCREMENT for table `tabel_user`
+
 ALTER TABLE `tabel_user`
   MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
